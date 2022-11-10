6bab46ab8023eb630bc338c27aa460cad728c40c
MIKKE-WANGO <mikemundati@gmail.com>
2022-11-10 11:47:50 +0300

    render setup

--- seraphic/settings.py
+++ seraphic/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'seraphic.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/seraphic',
         conn_max_age=600
     )
 }
