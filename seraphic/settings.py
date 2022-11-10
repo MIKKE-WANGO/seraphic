f4d7c5edee40a27bb69bb014f38433c229fee67e
MIKKE-WANGO <mikemundati@gmail.com>
2022-11-10 11:43:14 +0300

    render setup

--- seraphic/settings.py
+++ seraphic/settings.py
@@ -119,7 +119,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mikemundati@gmail.com'
-EMAIL_HOST_PASSWORD = 'owxohrxodimssrsg'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD ')
 EMAIL_USE_TLS = True
 
 # Password validation
